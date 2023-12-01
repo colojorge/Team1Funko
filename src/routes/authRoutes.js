3cb79457e58e67c51dbd0fd59514b769822bcd55
coco jorge <jorge_mattii@hotmail.com>
2023-11-30 23:46:57 -0300

    register resRender BD

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,11 +9,8 @@ const router = express.Router();
 router.get('/login', authRoutesControllers.login);
 router.post('/login', authRoutesControllers.postLogin);
 router.get('/register', authRoutesControllers.register);
-router.post('/register', authRoutesControllers.postRegister);
-// router.post('/register', async (req, res) => {
-//     const result = await postForm(req.body);
-//     console.log(result);
-// })
+router.post('/register', authRoutesControllers.postRegister)
+
 
 
 export default router;
