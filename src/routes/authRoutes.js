a9e686177c2088afc868fb52863eb23d03a32037
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2023-12-10 03:31:02 -0300

    v1 funcional

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,13 @@
-import express from 'express';
-import authRoutesControllers from '../../controllers/authRoutesController.js';
-import postForm from '../models/Formulario.js';
-import conn from '../config/conn.js';
-
+import express from "express";
+import authRoutesControllers from "../../controllers/authRoutesController.js";
+import postForm from "../models/Formulario.js";
+import conn from "../config/conn.js";
 
 const router = express.Router();
 
-router.get('/login', authRoutesControllers.login);
-router.post('/login', authRoutesControllers.postLogin);
-router.get('/register', authRoutesControllers.register);
-router.post('/register', authRoutesControllers.postRegister)
-
-
+router.get("/login", authRoutesControllers.login);
+router.post("/login", authRoutesControllers.postLogin);
+router.get("/register", authRoutesControllers.register);
+router.post("/register", authRoutesControllers.postRegister);
 
 export default router;
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import shopControllers from '../../controllers/shopController.js';
+import express from "express";
+import shopControllers from "../../controllers/shopController.js";
 
-const router = express.Router(); 
+const router = express.Router();
 
-router.get('/', shopControllers.shop);
-router.get('/item/:id', shopControllers.item);
-router.post('/item/:id/add', shopControllers.add);
-router.get('/cart', shopControllers.cart);
-router.post('/cart', shopControllers.postCart);
+router.get("/", shopControllers.shop);
+router.get("/item/:id", shopControllers.item);
+router.post("/item/:id/add", shopControllers.add);
+router.get("/carrito", shopControllers.cart);
+router.post("/carrito", shopControllers.postCart);
+router.get("*", shopControllers.notFound);
 
 export default router;
-
